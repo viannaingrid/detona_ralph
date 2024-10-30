986fa078a1bb4021de0702da5337bddc8b33ea72
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-10-29 22:32:13 -0300

    update js

---
+++ assets/js/engine.js
@@ -0,0 +1,13 @@
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values:{},
+};
+
+function init() {
+    
+}
