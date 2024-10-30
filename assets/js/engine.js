28d458608cde31df293b1fbf9c1ca731aadb8500
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-10-29 23:13:42 -0300

    update js

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -5,9 +5,34 @@ const state = {
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
     },
-    values:{},
+    values:{
+        timerId: null,
+        gameVelocity: 850
+    },
 };
 
-function init() {
-    
+function addListernerHitBox() {
+    state.view.squares.forEach(square) => {});
 }
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+
+    } );
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+
+    randomSquare.classList.add("enemy");
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function init() {
+    moveEnemy();
+}
+
+init();
